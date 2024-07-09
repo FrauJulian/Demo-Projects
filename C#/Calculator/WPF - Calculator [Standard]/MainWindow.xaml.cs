c779483f13e2f99d62997528679f1ef3daac4e85
Julian Lechner <fraujulian@lechner.top>
2024-07-09 15:23:38 +0200

    Update MainWindow.xaml.cs

--- C#/Calculator/WPF - Calculator [Standard]/MainWindow.xaml.cs
+++ C#/Calculator/WPF - Calculator [Standard]/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Data;
+using System.Data;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -7,9 +8,10 @@ namespace WPF___Calculator__Standard_
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+ 
     public partial class MainWindow : Window
     {
-        private string _operation = "-0";
+        private string _operation = "";
         private bool _equalsPressed = false;
 
         public MainWindow()
@@ -19,32 +21,24 @@ namespace WPF___Calculator__Standard_
 
         private void ButtonClick(object sender, RoutedEventArgs e)
         {
-            try
+            if (!_equalsPressed)
             {
-                if (!_equalsPressed)
-                {
-                    if (!_operation.EndsWith("+") ||
-                        !_operation.EndsWith("-") ||
-                        !_operation.EndsWith("*") ||
-                        !_operation.EndsWith("/") ||
-                        !_operation.EndsWith(","))
-                    {
-                        Button button = (Button)sender;
+                Button button = (Button)sender;
+                
+                string tmpOperation = _operation + button.Content;
 
-                        _operation = resultTextBox.Text + button.Content;
-                        resultTextBox.Text = Convert.ToString(_operation);
-                    }
+                if (ValidNumber(Convert.ToString(button.Content)))
+                {
+                    _operation = resultTextBox.Text + button.Content;
+                    resultTextBox.Text = Convert.ToString(_operation);
                 }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.ToString());
             }
         }
 
         private void EqualsClick(object sender, RoutedEventArgs e)
         {
-            if (!_equalsPressed)
+            if (!_equalsPressed &&
+                _operation != "")
             {
                 decimal result = Calculator(_operation);
                 resultTextBox.Text = resultTextBox.Text + " = " + result;
@@ -59,11 +53,23 @@ namespace WPF___Calculator__Standard_
             _equalsPressed = false;
         }
 
-        static decimal Calculator(string operation)
+        private static decimal Calculator(string operation)
         {
             DataTable dataTable = new DataTable();
             object result = dataTable.Compute(operation, null);
             return Convert.ToDecimal(result);
+        }
+
+        private static bool ValidNumber(string tmpOperation)
+        {
+            string regexPattern = @"^(\-)?(\d{1,6})?(\.{0,1})?(\d{1,3})?";
+            return Regex.IsMatch(tmpOperation, regexPattern);
+        }
+
+        private static bool ValidCalcType(string tmpOperation)
+        {
+            string regexPattern = @"^(\-)?(\d)?(\.)?(\d{1,3})*([+|\-|*|\/])?((\d)?(\.)?(\d{1,3})*)*";
+            return Regex.IsMatch(tmpOperation, regexPattern);
         }
     }
 }
